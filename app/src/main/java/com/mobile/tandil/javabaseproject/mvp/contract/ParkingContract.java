a0effeaaf92f89cd50352ef5adb54a1c19fa743c
AndresConfortiGL <andres.conforti@globant.com>
2022-07-22 17:58:34 -0300

    Added persistance
    fixed comments
    
    switched responsability from presenter to model
    
    Fixed responsabilities
    
    Fixed variable names
    
    Changed default values
    
    Used a String.format() to display values
    
    removed blank space

--- app/src/main/java/com/mobile/tandil/javabaseproject/mvp/contract/ParkingContract.java
+++ app/src/main/java/com/mobile/tandil/javabaseproject/mvp/contract/ParkingContract.java
@@ -2,11 +2,15 @@ package com.mobile.tandil.javabaseproject.mvp.contract;
 
 public interface ParkingContract {
     interface Model {
+        void setAvailableParkingSpots(int availableParkingSpots);
+        int getAvailableParkingSpots();
     }
 
     interface Presenter {
+        void showAvailableParkingSpots();
     }
 
     interface View {
+        void displayAvailableParkingSpots(String availableParkingSpots);
     }
 }
--- app/src/main/java/com/mobile/tandil/javabaseproject/mvp/model/ParkingModel.java
+++ app/src/main/java/com/mobile/tandil/javabaseproject/mvp/model/ParkingModel.java
@@ -4,4 +4,16 @@ import com.mobile.tandil.javabaseproject.mvp.contract.ParkingContract;
 
 public class ParkingModel implements ParkingContract.Model {
 
+    private static final int DEFAULT_VALUE = -1;
+    private int availableParkingSpots = DEFAULT_VALUE;
+
+    @Override
+    public void setAvailableParkingSpots(int availableParkingSpots){
+        this.availableParkingSpots = availableParkingSpots;
+    }
+
+    @Override
+    public int getAvailableParkingSpots(){
+        return availableParkingSpots;
+    }
 }
